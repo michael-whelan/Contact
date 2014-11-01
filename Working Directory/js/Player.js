195d1adb2a7819253e3a126a47b8eddbbecfb25c
Krentone <cjamieschmitz@gmail.com>
2014-11-01 07:32:34 -0400

    Updated Reload Sound
    Added metallic sound... testing to see how it sounds like.

--- Working Directory/js/Player.js
+++ Working Directory/js/Player.js
@@ -6,7 +6,7 @@ var spawnSnd = new Audio();
 //spawnSnd.src = "sounds/sfx/Player_Spawn.mp3";
 
 var reloadSnd = new Audio();
-//reloadSnd.src = "sounds/sfx/Gun_Recharge.mp3";
+//reloadSnd.src = "sounds/sfx/Gun_CRecharge.mp3";
 
 var Player=function (){
 	this.bullets =[];
